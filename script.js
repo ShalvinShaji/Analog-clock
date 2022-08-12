3aa10a255ad7e831a4559696067c0396e70fe942
Shalvin shaji <shalvinshaji283@gmail.com>
2022-08-12 16:12:31 +0530

    return to zero problem solved

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const HOURHAND = document.querySelector("#hour");
 const MINUTEHAND = document.querySelector("#minute");
 const SECONDHAND = document.querySelector("#second");
 
-function runClock(){
 
 var date = new Date();
 let hr= date.getHours();
@@ -14,9 +13,16 @@ let hrPosition=(hr*360/12)+(min*(360/60)/12);
 let minPosition=(min*360/60)+(sec*(360/60)/60);
 let secPosition= sec*360/60;
 
-HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
-MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
-SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
+function runClock(){
+    
+    hrPosition=hrPosition+(3/360)
+    minPosition=minPosition+(6/60) 
+    secPosition =secPosition+6;
+    
+    
+    HOURHAND.style.transform = "rotate(" + hrPosition + "deg)"
+    MINUTEHAND.style.transform = "rotate(" + minPosition + "deg)"
+    SECONDHAND.style.transform = "rotate(" + secPosition + "deg)"
 
 }
 
